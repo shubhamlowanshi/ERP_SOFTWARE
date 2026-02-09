d54e320336051ee3e3492b6d7485fb76c1994503
shubhamlowanshi2 <113835290+shubhamlowanshi@users.noreply.github.com>
2026-02-09 13:54:55 +0530

    feat-live

--- redux_toolkit/src/api/axios.js
+++ redux_toolkit/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+export const BASE_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL:"http://localhost:5000/api",
+  baseURL:`${BASE_URL}/api`,
    withCredentials: true,
     headers: {
     "Content-Type": "application/json", // ensure this
