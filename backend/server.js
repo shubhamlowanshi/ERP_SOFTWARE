c05e21b8db2d16cbd95da98c47259e0bb7e29ae0
shubhamlowanshi2 <113835290+shubhamlowanshi@users.noreply.github.com>
2026-02-10 09:29:31 +0530

    fix-corserror

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.options("*", cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/inventory", inventoryRoutes);
